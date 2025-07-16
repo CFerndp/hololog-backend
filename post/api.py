6449d309b5b35741969748460642b52194937c82
Cristian Fernandez <cristian.fernandez.del.pozo@gmail.com>
2025-07-16 17:47:33 +0200

    feat: add posts

--- post/api.py
+++ post/api.py
@@ -1,5 +1,5 @@
 from ninja import Router
-from post.schemas import PostSchema
+from post.schemas import PostSchema, PostCreateSchema
 from post.models import Post
 from ninja.pagination import paginate
 
@@ -9,3 +9,7 @@ router = Router()
 @paginate
 def get_posts(request):
     return Post.objects.all()
+
+@router.post("/")
+def create_post(request, post: PostCreateSchema):
+    return Post.objects.create(**post.dict())
--- post/schemas.py
+++ post/schemas.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-from ninja import Schema
+from ninja import Field, Schema
+from typing import Dict, Any
 
 class PostSchema(Schema):
     id: int
@@ -7,3 +8,19 @@ class PostSchema(Schema):
     content: str
     created_at: datetime
     updated_at: datetime
+    
+class PostCreateSchema(Schema):
+    title: str
+    content: Dict[str, Any] = Field (
+        description="Any valid JSON content. Can be used with rich text editors like Draft.js",
+        example={
+            "blocks": [
+                {
+                    "key": "foo",
+                    "text": "Example content",
+                    "type": "unstyled"
+                }
+            ]
+        }
+    )
+    
